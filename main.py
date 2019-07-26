998f20ca64cb1281f3f460bf5c418eac17fd228d
girinssh <tngus5851@naver.com>
2019-07-26 22:41:01 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from InstagramAPI import InstagramAPI
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+
+url = '''https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/''' + 'Gom1031' + '?api_key=RGAPI-29f8cf3d-25ab-4651-926d-ef1ebb40c76a'
